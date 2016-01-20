226333c3626a70cdbe75231d67352defbfe02c22
lzire <Vallee.victor@gmail.com>
2016-01-20 17:14:00 -0500

    Update winlogonExample.ps1

--- winlogonExample.ps1
+++ winlogonExample.ps1
@@ -4,9 +4,8 @@ $Global:WarningPreference = 'Continue'
 
 Import-Module "$(Split-Path -parent $MyInvocation.MyCommand.Path)\winlogonModule.psm1"
 
-LogInUser -username 'domain\username' -password 'myPassword'
+LogInUser
 
-# Do stuff here
-# Example : wmi requests etc.
+gwmi Win32_DiskDrive | select SerialNumber
 
 LogOffUser
